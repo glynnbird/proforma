75fd8299404f172489da7ffa4e987a05cbc04896
Glynn Bird <glynn.bird@gmail.com>
2015-07-12 09:01:30 +0100

    appcache update

--- js/proforma.js
+++ js/proforma.js
@@ -193,4 +193,16 @@ $( document ).ready(function() {
     $('#jumbo').hide();
     
   }, 3000);    
+  
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+  
 });
